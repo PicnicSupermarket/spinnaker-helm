a73371368ac1f4e5c1ffa13354f04de044d1f93b
Nathan Kooij <nathan.kooij@teampicnic.com>
2022-07-11 15:22:46 +0200

    PSM-1496 Introduce `deploy-chart.sh` to deploy the `spinnaker` chart to Nexus
    While there, introduce `.gitignore`.

---
+++ deploy-chart.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e -u -o pipefail
+
+NEXUS_USERNAME="${NEXUS_USERNAME:?NEXUS_USERNAME not specified or empty}"
+NEXUS_PASSWORD="${NEXUS_PASSWORD:?NEXUS_PASSWORD not specified or empty}"
+CHARTS_DIR="${1:-charts}"
+
+CHART_DIR="${CHARTS_DIR}/spinnaker"
+TMP_DIR="$(mktemp -d)"
+
+trap 'rm -rf -- "${TMP_DIR}"' INT TERM HUP EXIT
+helm3 package --dependency-update "${CHART_DIR}" --destination "${TMP_DIR}"
+find "${TMP_DIR}" -name '*.tgz' -exec \
+    curl --fail-with-body \
+    -u "${NEXUS_USERNAME}:${NEXUS_PASSWORD}" \
+    https://nexus.global.picnicinternational.com/repository/helm/ \
+    --upload-file {} +;
+
+echo "Published successfully"
